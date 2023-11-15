526c7836a08c41a831945300a77640da60f7dba9
Tbroskii <103056212+Tbroskii@users.noreply.github.com>
2023-11-14 21:06:24 -0600

    nice

---
+++ Develop/models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // The foreign key in the Product table
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // The foreign key in the Product table
+  onDelete: 'CASCADE', // Optional: cascade delete products when a category is deleted
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
